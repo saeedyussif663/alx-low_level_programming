f56d3cfe12dca1e4547a8c17c7ff0270ff64fdf9
saeedyussif663 <saeedyussif663@gmail.com>
2023-10-05 19:50:25 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#inlude <stdio.h>
+
+/** 
+ * main - this is a main function
+ *
+ * Returns: always 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0)
+}
