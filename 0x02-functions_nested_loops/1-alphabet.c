6577cf2e6eccdde28eb3f3847ccaf1d5df18495f
saeedyussif663 <saeedyussif663@gmail.com>
2023-10-10 07:11:24 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints all the alphabets,
+ * in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
