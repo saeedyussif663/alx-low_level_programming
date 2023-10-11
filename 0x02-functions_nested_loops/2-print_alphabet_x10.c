02db6db0ce9204d7d460918297a298e33c8cec59
saeedyussif663 <saeedyussif663@gmail.com>
2023-10-11 02:18:14 -0700

    putchar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,10 +2,8 @@
 
 /**
  * print_alphabet_x10 - print alphabet in lowercase 10x
- *
- * Return: 0 
+ * Return: 0
  */
-
 void print_alphabet_x10(void)
 {
 int i;
@@ -13,7 +11,7 @@ char j;
 
 for (i = 0 ; i < 10 ; i++)
 {
-for (j = 'a' ; j <= 'z' ; j++)	
+for (j = 'a' ; j <= 'z' ; j++)
 _putchar(j);
 _putchar('\n');
 }
