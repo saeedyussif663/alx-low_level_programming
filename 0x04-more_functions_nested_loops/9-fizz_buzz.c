6f98af6c641ff857049138dd93c6044552597bf9
Saeed Yussif <116543379+saeedyussif663@users.noreply.github.com>
2023-10-12 14:50:19 +0000

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
   * main - Prints numbers b/n 0-100
   *Return: Always 0
   */
-
 int main(void)
 {
 int i;
-
 for (i = 1; i <= 100; i++)
 {
 if ((i % 3) == 0 && (i % 5) == 0)
