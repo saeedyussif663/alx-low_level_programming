b665044ec2958c222707d43289bef3a8efedc540
Saeed Yussif <116543379+saeedyussif663@users.noreply.github.com>
2023-10-17 03:29:32 -0700

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,13 @@
 
 void puts2(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i % 2 == 0)
 {
 _putchar(str[i]);
-i += 2;
+} 
 }
 _putchar('\n');
 }
