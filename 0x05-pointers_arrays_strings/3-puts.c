8889ff6c2e18413919630d8cb6cb4a59a0f2404a
saeedyussif663 <saeedyussif663@gmail.com>
2023-10-16 18:24:31 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/** 
+ * __puts- prints a string followed by a new line
+ * @str: a pointer to the string
+ * Return: Always 0 (success)
+ */
+
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
