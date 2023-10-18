c28507796621db4d2b4cce0f60b5c017b6952f77
saeedyussif663 <saeedyussif663@gmail.com>
2023-10-18 12:42:22 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * strncat - concatnates two strings
+ * @dest: destination of concatenation
+ * @src: string to append
+ * @n: num of char to append
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+strncat(dest, src, n);
+return (dest)
+}
