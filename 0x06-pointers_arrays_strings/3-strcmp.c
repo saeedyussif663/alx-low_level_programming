e1037b9b11321bca2e449a2cc82992227f62ba9a
saeedyussif663 <saeedyussif663@gmail.com>
2023-10-18 13:52:08 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare to strings
+ * @s1: parameter
+ * @s2: parameter
+ * Return: interger
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i;
+i = strcmp(s1, s2)
+return (i);
+}
