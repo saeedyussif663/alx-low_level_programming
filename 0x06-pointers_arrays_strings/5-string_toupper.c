02fad5961883892ca206da2e2b61e1930b3fa92a
saeedyussif663 <saeedyussif663@gmail.com>
2023-10-19 03:50:34 -0700

    uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * string_toupper - chnage a string to uppercase
+ * @str: function parameter
+ * Return: str
+ */
+
+char *string_toupper(char *str)
+{
+int i;
+for (i = 0; i != '\0'; i++)
+{
+str[i] = toupper(str[i]);
+}
+
+return (str)
+}
