a1e42f718ef3e2723d5a44c6b9483639183e5c2a
Saeed Yussif <116543379+saeedyussif663@users.noreply.github.com>
2023-10-20 04:33:32 +0000

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,22 +8,21 @@
 
 char *leet(char *str)
 {
-  int i = 0;
-  int j = 0;
-  char s1[] = "aAeEoOtTlL";
-  char s2[] = "4433007711";
+int i = 0;
+int j = 0;
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
 
-while (str[i] != '\0')
-{
+while (str[i] != '\0') {
 while (j < 10) {
-if (str[i] == s1[j])
-{
+if (str[i] == s1[j]) {
 str[i] = s2[j];
 break;
 }
 j++;
 }
-
 i++;
 j = 0;
+}
+return (str);
 }
