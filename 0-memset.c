9af03959d7d1ba446212b08523563cf39ff2f183
saeedyussif663 <saeedyussif663@gmail.com>
2023-10-23 05:06:59 -0700

    0 memset

---
+++ 0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - fills the n byte
+ * @s: function parameter
+ * @b: function parameter
+ * @n: function parameter
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+memset(s, b, n);
+
+return(*s)
+}
