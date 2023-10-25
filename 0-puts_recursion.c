bdd559412f215db7e26a588bf2c33d519b95ffd0
saeedyussif663 <saeedyussif663@gmail.com>
2023-10-25 06:46:44 -0700

    task 0

---
+++ 0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * -puts_recursion - prints a string, followed by a new line
+ *
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+else
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+_putchar('\n');
+}
