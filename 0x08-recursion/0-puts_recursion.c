12a1fb490f497527fbf742be04571d67a48ff6ec
Saeed Yussif <116543379+saeedyussif663@users.noreply.github.com>
2023-10-25 07:17:42 -0700

    asa

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,14 @@
  * @s: parameter
  * Return: void
  */
+
 void _puts_recursion(char *s)
 {
-if (*s != '\0')
+if (*s == '\0')
 {
-_putchar(*s);
-_puts_recursion(s + 1);
+_putchar(10);
+return;
 }
-else
-_putchar('\n');
+_putchar(*s);
+_puts_recursion(++s);
 }
