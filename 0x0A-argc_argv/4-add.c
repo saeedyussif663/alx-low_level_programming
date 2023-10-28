8cebc5351bf10b45321680539585af58686c744b
saeedyussif663 <saeedyussif663@gmail.com>
2023-10-27 20:20:39 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: paramter
+ * @argv: parameter
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+
+}
