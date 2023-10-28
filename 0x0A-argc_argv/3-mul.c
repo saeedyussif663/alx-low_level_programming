a7fe569fc63640a3227ad22dabdaebde20da725d
Saeed Yussif <116543379+saeedyussif663@users.noreply.github.com>
2023-10-27 20:45:50 -0700

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-int i = 0, j = 0;
+int i = 0, j = 0; results = 0;
 
 if (argc == 3)
 {
 i = atoi(argv[1]);
 j = atoi(argv[2]);
-printf("%d\n", i * j);
+results = i * j;
+printf("%d\n", results);
 }
 else
 {
