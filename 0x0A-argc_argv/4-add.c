251356de8a855c34dfd859dfb4163aa17d2333e4
Saeed Yussif <116543379+saeedyussif663@users.noreply.github.com>
2023-10-27 20:24:49 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,5 +13,34 @@
 int main(int argc, char *argv[])
 {
 
+int i;
+unsigned int j, sum = 0;
+char *currentArg;
 
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+currentArg = argv[i];
+
+for (j = 0; j < strlen(currentArg); j++)
+{
+if (currentArg[j] < 48 || currentArg[j] > 57)
+{
+printf("Error\n");
+return (1);
+}
+}
+
+sum += atoi(currentArg);
+currentArg++;
+}
+
+printf("%d\n", sum);
+}
+else
+{
+printf("0\n");
+}
+	return (0);
 }
