c5a022bbed095218fe00a7ff0f69e296030ba215
Saeed Yussif <116543379+saeedyussif663@users.noreply.github.com>
2023-10-31 02:22:04 -0700

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,22 +12,18 @@ char *create_array(unsigned int size, char c)
 {
 char *ar;
 if(size == 0)
-{
 return (NULL);
-}
 
 ar = malloc(sizeof(c) * size);
 if (ar == NULL)
-{
 return (NULL);
-}
 else
 {
 unsigned int i;
 
 for (i = 0; i < size; i++)
-{
 ar[i] = c;
+
 }
-}
+return (ar);
 }
