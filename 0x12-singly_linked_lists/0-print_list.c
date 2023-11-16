0c64bb5bcf5f97eb3ff50f1a34c7e6c7650ede39
saeedyussif663 <saeedyussif663@gmail.com>
2023-11-16 15:25:16 +0000

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list -  elements of a linked list
+ * @h: parameter
+ * Return:  nodes printed
+ */
+size_t print_list(const list_t *h)
+{
+size_t z = 0;
+
+while (h)
+{
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+z++;
+}
+
+return (z);
+}
